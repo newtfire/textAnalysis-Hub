655c50f7c71f33e6c44302ba073473069f1a5e8a
ebeshero <ebbondar@gmail.com>
2023-02-06 14:05:11 -0500

    updated code, comments in radioscraper file to match grimmScraper

--- Class-Examples/Python/webScrapers/radioPlayScraper.py
+++ Class-Examples/Python/webScrapers/radioPlayScraper.py
@@ -1,10 +1,15 @@
-# 2021-03-03 ebb: I'm adapting a script for downloading videos from GeeksforGeeks.org: https://www.geeksforgeeks.org/downloading-files-web-using-python/
+# 2021-03-03 ebb: I adapted a script for downloading videos from GeeksforGeeks.org: https://www.geeksforgeeks.org/downloading-files-web-using-python/
+# Before beginning, you will need to do some pip installs at the command line:
+# go out to your shell (Git Bash or Terminal) and enter:
+# pip install BeautifulSoup4
+
 import bs4
 import requests
 # ebb: will lxml do just as well if not better?
 
 archive_url = "https://emruf.webs.com/"
-
+# Sadly, this site no longer exists. But I'm glad I was able to pull the
+# collection of radio plays from it, so we can use them in DIGIT 100 class!
 def get_radioplays():
     # create response object
     r = requests.get(archive_url)
@@ -19,8 +24,13 @@ def get_radioplays():
     hrefs = [archive_url + link['href'] for link in links]
 
     download_links(hrefs)
+    print("All radio plays downloaded!")
+    # When the download_links() function completes its work,
+    # this line will print.
 
 def download_links(hrefs):
+# Unlike in the grimmScraper file, I did not try to control where the output was stored.
+# To do that, adapt the script from grimmScraper
     for href in hrefs:
         # iterate through all links in hrefs
         # and download them one by one
@@ -29,10 +39,17 @@ def download_links(hrefs):
         # last string
         file_name = href.split('/')[-1]
 
-        print( "Downloading file:%s"%file_name)
+        print("Downloading file: " + file_name)
 
         # create response object
         r = requests.get(href, stream = True)
+
+        # If you want to send the output to a specific file directory, you will
+        # want to uncomment and adapt this code:
+        # workingDir = os.getcwd()
+        #     print("current working directory: " + workingDir)
+        #     fileDeposit = os.path.join(workingDir, 'YOUR-OUTPUT-FILE-DIRECTORY', file_name)
+        #     print(fileDeposit)
 
         # download started
         with open(file_name, 'wb') as f:
@@ -40,22 +57,14 @@ def download_links(hrefs):
                 if chunk:
                     f.write(chunk)
 
-        print( "%s downloaded!\n"%file_name )
-
-    print ("All radio plays downloaded!")
+        print("downloaded " + file_name)
     return
-
-
-
 
 # ebb: On the line if __name__ == "__main__": , see: https://medium.com/@j.yanming/debugging-from-main-to-main-in-python-fe2a9784b36
 if __name__ == "__main__":
 
     # getting all link to radio play files
     get_radioplays = get_radioplays()
-
-    # download all linked radio play files
-    # download_links(hrefs)
 
 
 
