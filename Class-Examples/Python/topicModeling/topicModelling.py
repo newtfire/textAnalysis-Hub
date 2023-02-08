28527a6bcd4919b98e79d837bc63bea367f00018
ebeshero <ebbondar@gmail.com>
2023-02-08 10:53:37 -0500

    updated Python examples for scraping and LDA topic modeling

--- Class-Examples/Python/topicModeling/topicModelling.py
+++ Class-Examples/Python/topicModeling/topicModelling.py
@@ -1,4 +1,5 @@
-# In the shell start with the following installs:
+# PIP INSTALLS TO MAKE FOR NLTK LDA TOPIC MODELING #################
+# In your Git Bash or Terminal, start with the following installs to your Python environment:
 # pip install gensim
 # pip install pyldavis
 # pip install nltk
@@ -11,20 +12,31 @@ import string
 import gensim.corpora as corpora
 from gensim.models import LdaModel
 import pyLDAvis.gensim_models
-# pyLDAvis.enable_notebook()
-from IPython.core.display import HTML
-# For visualizations try https://stackoverflow.com/questions/43014259/display-formatter-attribute-error-in-python
-# Simply doing visualisation = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary), and then pyLDAvis.save_html(visualisation, 'LDA_Visualization.html') works!
-# Other solutions:
-# https://github.com/bmabey/pyLDAvis/issues/101#issuecomment-743067299
-# pyLDAvis.show() works fine for me.
-# lda_display = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary, sort_topics=False)
-# pyLDAvis.show(lda_display)
 import os
 
 stop_words = stopwords.words('english')
-print(stop_words)
-print(string.punctuation)
+# print(stop_words)
+print(f"{stop_words=}")
+# ebb: Above line is an "f-string" or formatted print.
+# This is a fancy way of saying print(stop_words) only tell me WHAT you're printing.
+# When we do a lot of printing, it's helpful to have the information come out labeled.
+# f"{var=}" gives you a special "formatted print" in the console
+# and this formatting will let us tag the output as "stop words" since we
+# print a lot of output there.
+print(f"{string.punctuation=}")
+
+# ADDING WORDS TO THE STOP WORD LIST
+# ebb: You'll see some words that get repeated a lot, like "said" in your topic modeling
+# output. Adding these to the stop word list may improve the topic model clustering.
+# stop_words is just a list of words that we imported,
+# so you can just append a new value to the list like this:
+# stop_words = stop_words.append('said')
+# You can extend it to add a list of new stopwords with:
+# newStopWords = ["said", "one", "go", "went", "came"]
+# stop_words.extend(newStopWords)
+# print("UPDATED: " + f"{stop_words=}")
+# REMEMBER TO CHANGE YOUR EXTENDED STOP WORD LIST FOR NEW COLLECTIONS!
+
 
 # Set up to read our file directory ###
 workingDir = os.getcwd()
@@ -53,8 +65,6 @@ def clean_doc(doc):
     # final_words = [word for word in words if word not in stop_words]
     return final_words
 
-
-
 # ebb: This controls our file handling as a for loop over the directory:
 allDocs = []
 for file in os.listdir(grimmTalesPath):
@@ -65,6 +75,7 @@ for file in os.listdir(grimmTalesPath):
         # clean_doc(filepath)
 # print(allDocs)
 
+# PREPARING THE CORPUS FOR TOPIC MODELING ########################
 cleaned_docs = [clean_doc(doc) for doc in allDocs]
 id2word = corpora.Dictionary(cleaned_docs)
 
@@ -77,8 +88,7 @@ corpus = [id2word.doc2bow(cleaned_doc) for cleaned_doc in cleaned_docs]
 #     num = num[0]
 #     print(f"{num}\t{id2word[num]}")
 
-# TOPIC MODELING with LDA
-
+# TOPIC MODELING with LDA ########################
 lda_model = LdaModel(corpus=corpus, id2word=id2word, num_topics=20)
 # Suggestion: Try 10 - 50 topics and vary in 5s
 topics = lda_model.get_document_topics(corpus)
@@ -86,19 +96,20 @@ print(f"{len(topics)=}")
 # ebb: len(topics) appears to be the number of documents. There are
 # 209 documents in the Grimm collection.
 print(f"{topics[208]=}")
-# These are probably the topics in document 209. Remember why?
+# topics[208] represents the topics in document 209. Remember why?
 # Notice our format string: called "f-printing":
 # This comes out:
 # topics[208]=[(20, 0.11845379), (66, 0.015097282), (74, 0.118847124), (76, 0.31761664), (82, 0.42932528)]
 # Topics is a dictionary with the keys as the document numbers and values are the
-# list of topics for that document. The topics are tuples: Topic number, weight of topic.
+# list of topics for that document. The values are tuples: (Topic number, weight of topic).
 # HEY, don't we want to sort these by weight?
 sorted_topics = sorted(topics[208], key=lambda x: x[1], reverse=True)
 # This says, sort the topics, and the sort key is x, and then you'll get the second member
 print(f"{sorted_topics=}")
-# every time we run this, we get a different random assortment of topics present in the document we chose.
+# ebb: Notice, every time we run this, we get a different random assortment of topics present in the document we chose.
+# Our returns here are a sign of the randomization built into LDA topic modeling!
 
-
+# ebb: So, let's see what's in a topic:
 for topic in topics[208][:10]:
     # This asks for up to 10 topics in document 209. It'll be fine if 10 topic clusters aren't really available there.
     terms = lda_model.get_topic_terms(topic[0], 20)
@@ -114,11 +125,6 @@ for topic in topics[208][:10]:
 vis = pyLDAvis.gensim_models.prepare(lda_model, corpus, id2word, mds="mmds", R=30)
 # vis = pyLDAvis.show(vis)
 pyLDAvis.save_html(vis, 'topicModel_Visualization.html')
-
-
-
-
-
 
 
 
