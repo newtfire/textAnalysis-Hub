a3a3596da5752e41096986bc7a686bc392270cff
ebeshero <ebbondar@gmail.com>
2023-03-29 01:00:12 -0400

    LOTR Python stage 2, reading wtih SaxonC

---
+++ Class-Examples/Python/nlp-NER/py/lotr-files-stage2.py
@@ -0,0 +1,89 @@
+# STAGE 2: LET'S WORK WITH SELECT DATA FROM XML, PULLED WITH XPATH
+# pip install saxonche
+# The library above lets you read and pull data with XPath
+import os
+import spacy
+import re as regex
+from saxonche import PySaxonProcessor
+# ebb: The line above imports the PySaxonProcessor from SaxonCHE (free "home edition")
+# for work with XPath
+
+
+nlp = spacy.cli.download("en_core_web_lg")
+nlp = spacy.load('en_core_web_lg')
+# ebb: In the line above I'm loading one of the spaCy language models: use either _md or _lg.
+# If you change versions, you need to uncomment the line above and import it, and it can take
+# a little while to finish importing.
+# LOTR PROJECT NOTE: When I ran this with the _md language model, Frodo was being tagged as a PRODUCT.
+# The _lg model seemed more accurate (Frodo as PERSON)
+
+workingDir = os.getcwd()
+print("source-txt" + workingDir)
+
+insideDir = os.listdir(workingDir)
+print(str(insideDir))
+
+CollPath = os.path.join(workingDir, '../source-xml')
+print(CollPath)
+# 1. ebb: CollPath can also simply be defined more simply as a relative path
+# defined from this Python file's location, like this, because you climb up one directory
+# and then down into your source XML files:
+CollPath = '../source-xml'
+
+# 3. Here, the function imports each individual file, one at a time
+# (received from the for-loop below.
+def readTextFiles(filepath):
+    # with open(filepath, 'r', encoding='utf8') as f:
+    with PySaxonProcessor(license=False) as proc:
+        xml = open(filepath, encoding='utf-8').read()
+        # ebb: Here we changed to the Saxon processor to read files with XPath.
+        # From here on, we change how we formulate the string that Python will send to NLP.
+        xp = proc.new_xpath_processor()
+        node = proc.parse_xml(xml_text=xml)
+        xp.set_context(xdm_item=node)
+
+        xpath = xp.evaluate('//book//p ! normalize-space() => string-join()')
+        # ebb: Let's get the string() value of all the <p> elements that are descendants of <book>.
+        # The XPath function normalize-space() gets the string value and removes extra spaces.
+        # That way we avoid the prologue, preface material.
+        # I'm sending the whole thing to string-join() to bundle it together as one string
+        # instead of a new string for every <p> element.
+        string = xpath.__str__()
+        # print(string)
+
+
+        # ebb: Using REGEX to remove element tags for the moment so they don't get involved in the NLP.
+        # elementsRemoved = regex.sub('<.+?>', '', xpath)
+        # ebb: Now I don't have to remove elements because I pulled a string value out of my XML.
+        # Should we remove the single quotation marks? Not sure. If we do, uncomment the next line:
+        # cleanedUp = string.replace("'", '')
+        cleanedUp = regex.sub("(\.)([A-Z']])", "\1 \2", string)
+        # wbb: I noticed some sentences didn't have a space between end punctuation and the first word of the next sentence.
+        # print(cleanedUp)
+        tokens = nlp(cleanedUp)
+        # print(tokens)
+        listEntities = entitycollector(tokens)
+        # ebb: The line above sends our nlp tokens to the named entity collector function.
+        # THIS current function will receive and print a simple form of their output in the next line.
+        print(listEntities)
+
+# 4. ebb: The function below returns a simple list of named entities.
+# But on the way, we're printing out as much we can from spaCy's classification of named entities:
+def entitycollector(tokens):
+    entities = []
+    for entity in tokens.ents:
+        # if entity.label_ == "NORP" or entity.label_ == "LOC" or entity.label_=="GPE":
+        # ebb: The line helps experiment with different spaCy named entity classifiers, in combination if you like:
+        # When using it, remember to indent the next lines for the for loop.
+        print(entity.text, entity.label_, spacy.explain(entity.label_))
+        entities.append(entity.text)
+    return entities
+    # ebb: Keep the return line in position at same indentation level as the definition of the entities variable.
+
+# 2. ebb: The for loop below is working with your CollPath, and going through each file inside,
+# and sending it up to readTextFiles, where the nlp processing will happen.
+for file in os.listdir(CollPath):
+    if file.endswith(".xml"):
+        filepath = f"{CollPath}/{file}"
+        # print(filepath)
+        readTextFiles(filepath)
